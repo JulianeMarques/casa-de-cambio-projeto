ac234471f239172e9a237b80e884ee3dc2690ad8
Juliane Marques <mqs.juliane@gmail.com>
2021-07-12 11:38:48 -0300

    HTML e JS prontos

--- script.js
+++ script.js
@@ -1,39 +1,67 @@
-/* fetch('https://api.exchangerate.host/latest?base=BRL')
-    .then((respose) => respose.json())
-    .then(({ base, rates }) => console.log(base, rates)) */ 
-// essa ultima parte da funcao acima nao precisa ser incluida na funcao fetchCurrency pois la so queremos retornar os dados do JSON
+const currencyList = document.querySelector('#currency-list');
+const currencyBase = document.querySelector('#currency-base');
 
-function fetchCurrency(base = 'BRL') { // base = BRL ja deixa como padrao (chamado de parent default)
-    return fetch(`https://api.exchangerate.host/latest?base=${base}`) // precisamos adcionar o return para que retorne uma promise ja que estamos dentro de uma funcao
-    .then((respose) => respose.json());
+async function fetchCurrency(base) { // base = BRL ja deixa como padrao (chamado de parent default)
+    const response = await fetch(`https://api.exchangerate.host/latest?base=${base}`); // precisamos adcionar o return para que retorne uma promise ja que estamos dentro de uma funcao
+    console.log(response);
+    const data = await response.json();
+    return data;
 };
+
+const addCurrency = ([currency, value]) => { // nesse ponto utilizando [] desestruturamos o array e os valores sao exibidos do jeito que queremos para o usuario
+    const newLi = document.createElement('li'); // passa por parametro a li que vamos criar
+    newLi.innerText = `${currency}: ${value}`;
+    currencyList.appendChild(newLi);
+};
+
+const clearCurrencyList = () => {
+    currencyList.innerHTML = '';
+  }
+
+  const populateCurrencyList = (rates) => {
+    Object.entries(rates) // bloquear o padrao do input
+    .forEach(addCurrency)
+  }
+  
+/*     fetchCurrency(inputCurrency.value)
+        .then(({ base, rates }) => {
+            console.log(base);
+            console.log(rates);  consoles feitos para entender o que cada um faz/traz
+            // console.log(Object.entries(rates)); // transforma o objeto em array com chave e valor
+            // Object.entries(rates).forEach((value) => { // exibe bonitinho chave e valor e precisamos de desestruturar o array para pegar cada um dos dados
+            currencyBase.innerText = `Valores referentes a 1 ${base}`;
+            clearCurrencyList();
+            Object.entries(rates).forEach(addCurrency);
+            });
+        }; */
+
+const clickHandle = async (event) => {
+        event.preventDefault();
+        const inputCurrency = document.querySelector('#currency-input');
+          
+        try {
+            const baseValue = inputCurrency.value;
+             if (baseValue === '') {
+             throw new Error('Informe alguma moeda');
+            }
+            const { base, rates } = await fetchCurrency(baseValue);
+              currencyBase.innerText = `Valores referentes a 1 ${base}`;
+              clearCurrencyList();
+              populateCurrencyList(rates);
+            } catch (error) {
+              alert(`Deu ruim! ${error}`);
+            }
+          
+          }        
 
 const setupEventHandlers = () => {
     const searchButton = document.querySelector('#search-button'); // pega botao
 
-    searchButton.addEventListener('click', (event) => {
-        event.preventDefault(); // bloquear o padrao do input
-        const inputCurrency = document.querySelector('#currency-input'); // pega o input
-        fetchCurrency(inputCurrency.value)
-            .then(({ base, rates }) => {
-                /* console.log(base);
-                console.log(rates);  consoles feitos para entender o que cada um faz/traz */
-                // console.log(Object.entries(rates)); // transforma o objeto em array com chave e valor
-                //Object.entries(rates).forEach((value) => { // exibe bonitinho chave e valor e precisamos de desestruturar o array para pegar cada um dos dados
-                Object.entries(rates).forEach(([currency, value]) => { // nesse ponto utilizando [] desestruturamos o array e os valores sao exibidos do jeito que queremos para o usuario
-                    const currencyList = document.querySelector('#currency-list');
-                    const newLi = document.createElement('li'); // passa por parametro a li que vamos criar
-                    newLi.innerText = `${currency}: ${value}`;
-                    currencyList.appendChild(newLi);
-                    console.log(currency, value);
-                });
-                });
-            });
-    };
+    searchButton.addEventListener('click', clickHandle);
+}
 
 
 
-    window.onload = () => { // atribuir as funcoes que quero que aparecam para o usuario (boa pratica)
+window.onload = () => { // atribuir as funcoes que quero que aparecam para o usuario (boa pratica)
     setupEventHandlers();
-    fetchCurrency();
-    };
+}
